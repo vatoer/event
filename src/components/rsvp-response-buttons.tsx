bbda4bb0fe1d7f8cabdb8db9aafaa84f59452f1e
vatoer <vatoer.ckplus@gmail.com>
2024-09-03 20:56:48 +0200

    update button

--- src/components/rsvp-response-buttons.tsx
+++ src/components/rsvp-response-buttons.tsx
@@ -5,55 +5,44 @@ import { useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 
 interface RsvpResponseButtonsProps {
-    options: {
-        text: string;
-        rsvpResponse: RsvpResponse;
-      }[];
-      selectedOption: string | null;
-      setSelectedOption: (option: string) => void;}
+  options: {
+    text: string;
+    rsvpResponse: RsvpResponse;
+  }[];
+  selectedOption: string | null;
+  setSelectedOption: (option: string) => void;
+}
 
 const RsvpResponseButtons = ({
-    options,
-    selectedOption,
-    setSelectedOption,
+  options,
+  selectedOption,
+  setSelectedOption,
 }: RsvpResponseButtonsProps) => {
-    const touchHandled = useRef(false);
+  const handleClick = (option: string) => {
+    setSelectedOption(option);
+  };
 
-    const handleClick = (option: string) => {
-        if (!touchHandled.current) {
-            setSelectedOption(option);
-          }
-          touchHandled.current = false; // Reset the flag after handling click
-    };
-
-    const handleTouchEnd = (option: string) => {
-        touchHandled.current = true;
-        setSelectedOption(option);
-      };
-
-    return (
-        <div className="w-full flex flex-col items-center p-2">
-          <div className="flex flex-col gap-2 w-full">
-            {options.map((option) => (
-              <button
-                //variant={"outline"}
-                key={option.text}
-                onClick={() => handleClick(option.rsvpResponse)}
-                onTouchEnd={() => handleTouchEnd(option.rsvpResponse)}
-                className={cn(
-                    "w-full p-2 rounded-md border border-customRed transition-colors sm:hover:bg-red-300 sm:hover:text-white",
-                    selectedOption === option.rsvpResponse
-                        ? "bg-customRed text-white"
-                        : "")
-                }
-                
-              >
-                {option.text}
-              </button>
-            ))}
-          </div>
-        </div>
-      );
+  return (
+    <div className="w-full flex flex-col items-center p-2">
+      <div className="flex flex-col gap-2 w-full">
+        {options.map((option) => (
+          <button
+            //variant={"outline"}
+            key={option.text}
+            onClick={() => handleClick(option.rsvpResponse)}
+            className={cn(
+              "w-full p-2 rounded-md border border-customRed transition-colors sm:hover:bg-red-300 sm:hover:text-white",
+              selectedOption === option.rsvpResponse
+                ? "bg-customRed text-white"
+                : ""
+            )}
+          >
+            {option.text}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RsvpResponseButtons;
